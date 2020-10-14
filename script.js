26b7549a45e26029dcbbf055fdb4eb9320c64c2e
inapanuri <inapanurimanojkumar@gmail.com>
2020-10-14 22:40:31 +0530

    2 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function fun(){
-  var val1=Number(document.getElementById("id1").value);
-  var val2=Number(document.getElementById("id2").value);
+  const val1=Number(document.getElementById("id1").value);
+  const val2=Number(document.getElementById("id2").value);
   const ele=document.createElement("div");
-  ele.innerHTML="X+Y="+(val1+val2)
+  const sum=val1+val2;
+  if(sum.toFixed(2)==="58.01")//truncate to 2 decimal points
+  //floating point comparison
+  //triple equals  used to comparison equal to compare values both type and value
+  // 5 =="5" is true in JS but  5==="5" if false in JS
+  // It is better to use === always in JS
+  {
+    ele.innerHTML="Correct = "+("58.01");
+  }
+  else{
+    ele.innerHTML="Incorrect="+(sum);
+  }
   ele.style.backgroundColor="lightblue";
   const containingElement=document.body;
   containingElement.appendChild(ele);
